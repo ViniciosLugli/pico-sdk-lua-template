90bae822145024b41a9ebee6ca5df32fbdb2cc17
ViniciosLugli <vinicioslugli@gmail.com>
2024-03-26 17:03:14 -0300

    feat(project): :tada: Upload base project template with lua 5.5

--- include/test.hpp
+++ include/test.hpp
@@ -1,9 +1,21 @@
-#include <hardware/clocks.h>
-
+extern "C" {
+#include <lauxlib.h>
+#include <lua.h>
+#include <lualib.h>
+}
 void test_function(void) {
-	printf("RP2040:\n");
+	lua_State *L = luaL_newstate();
+	luaL_openlibs(L);
 
-	uint32_t clock_hz = clock_get_hz(clk_sys);
-	float clock_mhz = clock_hz / 1000000.0f;
-	printf("System clock: %.2f MHz\n", clock_mhz);
+	const char *lua_version = lua_pushfstring(L, "%s", LUA_VERSION);
+	printf("Lua version: %s\n", lua_version);
+
+	const char *lua_script = "print('Hello, world!')";
+	int lua_result = luaL_dostring(L, lua_script);
+	if (lua_result != LUA_OK) {
+		const char *error_message = lua_tostring(L, -1);
+		printf("Error: %s\n", error_message);
+	}
+
+	lua_close(L);
 }
